f3f7ddcebc4bff9a56585973f0113bb0d5faecc6
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2022-03-25 12:19:23 -0700

    Added image rotator

--- src/lib/globals/Nav.svelte
+++ src/lib/globals/Nav.svelte
@@ -124,8 +124,8 @@
 		</div>
 	</a>
 </div>
-<nav class="fixed top-8 right-0 mix-blend-difference z-50 hidden md:block">
-	<ul class="nav-links flex flex-col items-end">
+<nav class="nav-links fixed top-0 pt-8 pl-4 right-0 mix-blend-difference z-50 hidden md:block">
+	<ul class="flex flex-col items-end">
 		{#each links as link}
 			<li class="relative mb-2 border-r-8 border-white">
 				<a class="group" href={link.href}>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,22 @@
-<section class="relative h-screen bg-black">
-	<div class="absolute top-0 left-0 w-full h-64 bg-gradient-to-b from-black opacity-60 z-10" />
-	<img
-		class="w-full h-full object-cover opacity-50"
-		src="https://teague.com/uploads/default/_2400x1585_crop_center-center_75_line/Hannah-Starbucks.jpg"
-		alt="fpo"
-	/>
+<script>
+	let src = [
+		'https://teague.com/uploads/default/_2400x1585_crop_center-center_75_line/Hannah-Starbucks.jpg',
+		'https://teague.com/uploads/featured/_1536x864_crop_center-center_75_line/Starbucks_Customer_Experience_Illustration.png',
+		'https://teague.com/uploads/featured/_1984x1240_crop_center-center_75_line/Virgin-Hyperloop-Header.jpg',
+		'https://teague.com/uploads/default/_1536x864_crop_center-center_75_line/UX-Landing-Page-Header.jpg',
+		'https://teague.com/uploads/default/_1200x702_crop_center-center_100_line/AirSpace-Landing-Page-Header.jpg'
+	];
+	let i = 0;
+	function inc() {
+		i >= src.length - 1 ? (i = 0) : i++;
+	}
+</script>
+
+<section class="relative h-screen bg-black" on:click={inc}>
+	<div class="absolute top-0 left-0 w-full h-64 bg-gradient-to-b from-black opacity-30 z-10" />
+	<div class="absolute top-0 right-0 w-64 h-full bg-gradient-to-l from-black opacity-30 z-10" />
+	<img class="w-full h-full object-cover opacity-50" src={src[i]} alt="fpo" />
+	<p class="relative z-50">{i}</p>
 </section>
 <section class="min-h-screen bg-white px-4 md:px-12 lg:px-20">
 	<p>
