0fdd644f4680944ade842295038103a3b67b9e44
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2022-04-05 11:32:16 -0700

    added refresh icon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,8 +17,22 @@
 	<div class="absolute top-0 right-0 w-64 h-full bg-gradient-to-l from-black opacity-30 z-10" />
 	<img class="w-full h-full object-cover opacity-50" src={src[i]} alt="fpo" />
 	<button
-		class="absolute bottom-12 left-4 md:left-12 lg:left-20 bg-white font-bold font-lg rounded-full py-4 px-6 transition-colors duration-300 hover:bg-magenta hover:text-white"
-		on:click={inc}>Swap Image</button
+		class="absolute bottom-12 left-4 flex gap-x-2 md:left-12 lg:left-20 bg-white font-bold font-lg rounded-full py-4 px-6 transition-colors duration-300 hover:bg-magenta hover:text-white"
+		on:click={inc}
+		><svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			stroke-width="2"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+			/>
+		</svg>Swap Image</button
 	>
 </section>
 <section class="min-h-screen bg-white py-28 px-4 md:px-12 lg:px-20">
